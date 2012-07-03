9e1da66fdd473cd472f3bccc892ae9f9253883b7
Markus Olsson <j.markus.olsson@gmail.com>
2012-07-03 11:05:11 +0200

    Initial import from freakcode.com

---
+++ MemberwiseEqualityIgnoreAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace freakcode.Utils
+{
+    /// <summary>
+    /// Tells the MemberwiseEqualityComparer to ignore a specific field when performing
+    /// the comparison. The field is also excluded from the hashcode computation.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+    public class MemberwiseEqualityIgnoreAttribute : Attribute
+    {
+    }
+}
